8822311e95c793bfe217b95e783744a21eb27b6f
dracero <dracero>
2023-12-11 21:31:50 -0300

    pwa

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,56 @@
-import { defineConfig } from 'astro/config';
-
+import { defineConfig } from 'astro/config'
+import AstroPWA from '@vite-pwa/astro'
 
 // https://astro.build/config
-export default {
-    output: 'hybrid', // o 'hybrid'
-    // ...resto de tu configuración...
-  }
+export default defineConfig({
+  output: 'hybrid', 
+  vite: {
+    logLevel: 'info',
+    define: {
+      __DATE__: `'${new Date().toISOString()}'`,
+    },
+  },
+  integrations: [
+    AstroPWA({
+      mode: 'development',
+      base: '/',
+      scope: '/',
+      includeAssets: ['favicon.svg'],
+      registerType: 'autoUpdate',
+      manifest: {
+        name: 'Astro PWA',
+        short_name: 'Astro PWA',
+        theme_color: '#ffffff',
+        icons: [
+          {
+            src: 'pwa-192x192.png',
+            sizes: '192x192',
+            type: 'image/png',
+          },
+          {
+            src: 'pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+          },
+          {
+            src: 'pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'any maskable',
+          },
+        ],
+      },
+      workbox: {
+        navigateFallback: '/',
+        globPatterns: ['**/*.{css,js,html,svg,png,ico,txt}'],
+      },
+      devOptions: {
+        enabled: true,
+        navigateFallbackAllowlist: [/^\//],
+      },
+      experimental: {
+        directoryAndTrailingSlashHandler: true,
+      }
+    }),
+  ],
+})
