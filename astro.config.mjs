3d6391e3f68c75c7ff87981f13c412c7784d4057
dracero <dracero@fi.uba.ar>
2023-12-13 15:26:14 -0300

    pwatest

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,57 +1,54 @@
-import { defineConfig } from 'astro/config'
-import AstroPWA from '@vite-pwa/astro'
+import { defineConfig } from 'astro/config';
+import AstroPWA from '@vite-pwa/astro';
+
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [
-    AstroPWA({
-      mode: 'production',
-      base: '/',
-      scope: '/',
-      includeAssets: ['favicon.svg'],
-      registerType: 'autoUpdate',
-      manifest: {
-        name: 'Astro PWA',
-        short_name: 'Astro PWA',
-        theme_color: '#ffffff',
-        icons: [
-          {
-            src: 'pwa-192x192.png',
-            sizes: '192x192',
-            type: 'image/png',
-          },
-          {
-            src: 'pwa-512x512.png',
-            sizes: '512x512',
-            type: 'image/png',
-          },
-          {
-            src: 'pwa-512x512.png',
-            sizes: '512x512',
-            type: 'image/png',
-            purpose: 'any maskable',
-          },
-        ],
-      },
-      workbox: {
-        navigateFallback: '/',
-        globPatterns: ['**/*.{css,js,html,svg,png,ico,txt}'],
-      },
-      devOptions: {
-        enabled: true,
-        type: 'module',
-        navigateFallbackAllowlist: [/^\//],
-      },
-      experimental: {
-        directoryAndTrailingSlashHandler: true,
-      }
-    }),
-  ],
-  output: 'hybrid', 
+  integrations: [AstroPWA({
+    mode: 'production',
+    base: '/',
+    scope: '/',
+    includeAssets: ['favicon.svg'],
+    registerType: 'autoUpdate',
+    manifest: {
+      name: 'Astro PWA',
+      short_name: 'Astro PWA',
+      theme_color: '#ffffff',
+      icons: [{
+        src: 'pwa-192x192.png',
+        sizes: '192x192',
+        type: 'image/png'
+      }, {
+        src: 'pwa-512x512.png',
+        sizes: '512x512',
+        type: 'image/png'
+      }, {
+        src: 'pwa-512x512.png',
+        sizes: '512x512',
+        type: 'image/png',
+        purpose: 'any maskable'
+      }]
+    },
+    workbox: {
+      navigateFallback: '/',
+      globPatterns: ['**/*.{css,js,html,svg,png,ico,txt}']
+    },
+    devOptions: {
+      enabled: true,
+      type: 'module',
+      navigateFallbackAllowlist: [/^\//]
+    },
+    experimental: {
+      directoryAndTrailingSlashHandler: true
+    }
+  })],
+  output: 'hybrid',
   vite: {
     logLevel: 'info',
     define: {
-      __DATE__: `'${new Date().toISOString()}'`,
-    },
+      __DATE__: `'${new Date().toISOString()}'`
+    }
   },
-})
+  adapter: vercel()
+});
